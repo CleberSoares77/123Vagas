1a85007c5d8a93e7191b8635026183fc97dc2475
cleber <clebersoares749@gmail.com>
2024-03-20 23:06:51 -0300

    .

--- application/views/template.php
+++ application/views/template.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
-	<link rel="stylesheet" href="<?= base_url('assets/css/style.min.css'); ?>">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.min.css'); ?>">
 </head>
 <body>
 
 <div class="container">
-	<!-- MENU -->
-	<p><a href="<?= base_url('login'); ?>">Login</a> | <a href="<?= base_url('cadastro-usuario');
-	  ?>">Cadastro usuário</a> | <a href="<?= base_url('cadastro-empresa'); ?>">Cadastro empresa</a></p>
+    <!-- MENU -->
+	<div class="d-flex justify-content-end mt-5">
+    <a href="<?= base_url('login'); ?>" class="btn btn-primary mr-5">Login</a>
+    <a href="<?= base_url('cadastro-usuario'); ?>" class="btn btn-success mx-5">Cadastro usuário</a>
+    <a href="<?= base_url('cadastro-empresa'); ?>" class="btn btn-info ml-5">Cadastro empresa</a>
+</div>
 
-	<!-- CONTEÚDO -->
-	<?= $contents; ?>
+
+    <!-- CONTEÚDO -->
+    <?= $contents; ?>
 </div>
 
 <script src="<?= base_url('assets/js/jquery.min.js'); ?>"></script>
