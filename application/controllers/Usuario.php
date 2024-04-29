77d9c93eb3d85d8350351e57b79d6ef111a486d8
cleber <clebersoares749@gmail.com>
2024-04-28 23:36:29 -0300

    .

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -29,9 +29,50 @@ class Usuario extends CI_Controller
 	}
 
 	public function recuperar_senha()
-	{
-		$this->load->view('recuperar_senha');
-	}
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Lógica para recuperar a senha e obter o email do usuário
+        $email = $this->input->post('email'); // Supondo que o email foi recuperado
+        
+        // Verifique se o email está correto e envie o email de recuperação
+        if ($email != '') {
+            // Gerar um token único para o usuário
+            $token = bin2hex(random_bytes(16)); // Gera um token de 32 caracteres
+            
+            // Salvar o token no banco de dados junto com o email e a data de expiração (opcional)
+            $this->Usuario_model->salvarTokenRedefinicaoSenha($email, $token);
+
+            // Construir o link de redefinição de senha
+            $linkRedefinicao = base_url('usuario/recuperar_senha') . '?email=' . urlencode($email) . '&token=' . urlencode($token);
+
+            // Construir o email
+            $to = $email;
+            $subject = "Recuperação de Senha";
+            $message = "Olá,<br><br>Você solicitou a recuperação de senha. Clique no link abaixo para redefinir sua senha:<br><br><a href=\"$linkRedefinicao\">Redefinir Senha</a><br><br>Atenciosamente,<br>Equipe do Sistema";
+
+            // Cabeçalho do email
+            $header = "From: clebersoares749@gmail.com \r\n";
+            $header .= "Content-Type: text/html; charset=UTF-8\r\n"; // Definindo o tipo de conteúdo como HTML
+            
+            // Envio do email de recuperação de senha
+            $result = mail($to, $subject, $message, $header);
+            
+            // Verifica se o email foi enviado com sucesso
+            if ($result) {
+                echo 'Email de recuperação de senha enviado com sucesso';
+            } else {
+                echo 'Ocorreu um erro ao enviar o email de recuperação de senha';
+            }
+        } else {
+            echo 'Email não fornecido para recuperação de senha';
+        }
+    }
+
+    $this->load->view('recuperar_senha');
+}
+
+
+	
 
 	public function cadastro()
 	{
@@ -45,17 +86,32 @@ class Usuario extends CI_Controller
 				'genero' => $this->input->post('genero'),
 				'senha' => $this->input->post('senha')
 			];
-
+	
 			// Efetua o cadastro e valida se foi cadastrado
 			if ($this->Usuario_model->cadastrar($data)) {
-				echo 'Cadastrou';
+				// Envio do email após o cadastro
+				$to = $data['email'];
+				$subject = "Bem-vindo ao nosso sistema";
+				$message = "Olá " . $data['nome'] . ",\r\n\r\nSeu cadastro foi realizado com sucesso!\r\n\r\nAtenciosamente,\r\nEquipe do Sistema";
+				$header = "From: seu-email@exemplo.com \r\n";
+				
+				// Envio do email
+				$result = mail($to, $subject, $message, $header);
+				
+				// Verifica se o email foi enviado com sucesso
+				if ($result) {
+					echo 'Cadastrou e enviou email de boas-vindas';
+				} else {
+					echo 'Cadastrou, mas ocorreu um erro ao enviar o email';
+				}
 			} else {
 				echo 'Ocorreu um erro ao cadastrar';
 			}
 		}
-
+	
 		$this->template->load('template', 'usuario/cadastro');
 	}
+	
 
 	public function editar($id)
 	{
@@ -139,6 +195,8 @@ class Usuario extends CI_Controller
 		}
 	}
 
+	
+
 	public function validation()
 	{
 		$this->load->model('Usuario_model');
@@ -150,4 +208,6 @@ class Usuario extends CI_Controller
 			return false;
 		}
 	}
+
+	
 }
--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -55,6 +55,37 @@ class Usuario_model extends CI_Model
             return false;  
         }  
   
-    }  
+    }
+	
+	// Método para atualizar a senha do usuário
+    public function atualizarSenha($email, $novaSenha)
+    {
+        // Verificar se o usuário com o email fornecido existe
+        $this->db->where('email', $email);
+        $query = $this->db->get('cadastro_usuario');
+
+        if ($query->num_rows() > 0) {
+            // Atualizar a senha do usuário
+            $data = array(
+                'senha' => password_hash($novaSenha, PASSWORD_DEFAULT) // Use password_hash para criptografar a nova senha
+            );
+            $this->db->where('email', $email);
+            $this->db->update('cadastro_usuario', $data);
+
+            return true; // Indica que a senha foi atualizada com sucesso
+        } else {
+            return false; // Indica que o usuário não foi encontrado
+        }
+    }
+
+	public function salvarTokenRedefinicaoSenha($email, $token)
+    {
+        $data = array(
+            'token_redefinicao_senha' => $token,
+            'data_expiracao_token' => date('Y-m-d H:i:s', strtotime('+1 hour')) // Exemplo: token expira em 1 hora
+        );
+        $this->db->where('email', $email);
+        $this->db->update('cadastro_usuario', $data);
+    }
   
 }
