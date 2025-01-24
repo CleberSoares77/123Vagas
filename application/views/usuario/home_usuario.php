bb4b8bbfcaf90e9884f4ad1469905788e1f6f6ae
cleber <clebersoares749@gmail.com>
2025-01-23 21:12:00 -0300

    teste

--- application/views/usuario/home_usuario.php
+++ application/views/usuario/home_usuario.php
@@ -15,7 +15,7 @@
 <body>
 
   <!-- Sidebar -->
-  <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:8%">
+  <!--<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:8%">-->
     <h3 class="w3-bar-item">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
