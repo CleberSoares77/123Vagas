d283a88522c90179425aaca79c8182539af1b334
cleber <clebersoares749@gmail.com>
2025-01-23 21:22:09 -0300

    teste 1

---
+++ application/views/usuario/vagas.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+ <meta charset="UTF-8">
+ <title>Vagas</title>
+ <style>
+   table {
+     border-collapse: collapse;
+     width: 80%;
+   }
+   th, td {
+     border: 1px solid black;
+     padding: 8px;
+     text-align: left;
+   }
+ </style>
+</head>
+<body>
+  <h5>Adicionar vagas</h5>
+
+  <table border="1">
+   <thead>
+           
+   </thead>
+  </table>
+</body>
+</html>
