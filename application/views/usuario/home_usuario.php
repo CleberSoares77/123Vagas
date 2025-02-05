d701f75481588e637fc70cccc6ecd8a7f58bca88
cleber <clebersoares749@gmail.com>
2025-02-05 14:18:26 -0300

    .

--- application/views/usuario/home_usuario.php
+++ application/views/usuario/home_usuario.php
@@ -12,6 +12,20 @@
 </style>
 
 <body>
-ola!
+<?php if (!empty($vagas)) : ?>
+    <?php foreach ($vagas as $vaga) : ?>
+        <div class="w3-col m4">
+            <div class="w3-card">
+                <img src="<?= base_url($vaga->imagem) ?>" alt="<?= $vaga->nome ?>" style="width:100%">
+                <div class="w3-container">
+                    <h4><b><?= $vaga->nome ?></b></h4>
+                    <p><?= $vaga->descricao ?></p>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+<?php else : ?>
+    <p>Nenhuma vaga cadastrada.</p>
+<?php endif; ?>
 </body>
 </html>
