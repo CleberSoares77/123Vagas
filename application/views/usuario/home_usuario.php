19dcc908a746ea4cf871afd18735bd3ed07d9bf5
cleber <clebersoares749@gmail.com>
2025-02-05 15:07:25 -0300

    .

--- application/views/usuario/home_usuario.php
+++ application/views/usuario/home_usuario.php
@@ -12,22 +12,25 @@
 </style>
 
 <body>
-<?php if (isset($vagas) && !empty($vagas)) : ?>
-    <div class="w3-row-padding">
+  <div class="w3-container">
+    <h2>Vagas Cadastradas</h2>
+    <?php if (!empty($vagas)) : ?>
+      <div class="w3-row-padding">
         <?php foreach ($vagas as $vaga) : ?>
-            <div class="w3-col m4">
-                <div class="w3-card">
-                    <img src="<?= base_url($vaga->imagem) ?>" alt="<?= $vaga->titulo ?>" style="width:100%">
-                    <div class="w3-container">
-                        <h4><b><?= htmlspecialchars($vaga->titulo) ?></b></h4>
-                        <p><?= nl2br(htmlspecialchars($vaga->descricao)) ?></p>
-                    </div>
-                </div>
+          <div class="w3-col m4">
+            <div class="w3-card">
+              <img src="<?= base_url($vaga->imagem) ?>" alt="<?= htmlspecialchars($vaga->titulo) ?>" style="width:100%">
+              <div class="w3-container">
+                <h4><b><?= htmlspecialchars($vaga->titulo) ?></b></h4>
+                <p><?= nl2br(htmlspecialchars($vaga->descricao)) ?></p>
+              </div>
             </div>
+          </div>
         <?php endforeach; ?>
-    </div>
-<?php else : ?>
-    <p>Nenhuma vaga cadastrada.</p>
-<?php endif; ?>
+      </div>
+    <?php else : ?>
+      <p>Nenhuma vaga cadastrada.</p>
+    <?php endif; ?>
+  </div>
 </body>
 </html>
