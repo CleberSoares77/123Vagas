933ec472589f20df4f39f502d80eaa16ac365c10
cleber <clebersoares749@gmail.com>
2025-02-05 15:47:28 -0300

    .

--- application/views/index.php
+++ application/views/index.php
@@ -115,7 +115,7 @@
                         <h3 class="register-heading">Login Usuário</h3>
                         <div class="row register-form">
                             <div class="col-md-12">
-                                <form id="formUsuario" method="post" action="<?php echo site_url('/usuario/homeUsuario'); ?>">
+                                <form id="formUsuario" method="post" action="<?php echo site_url('Usuario/login_action'); ?>">
                                     <div class="form-group">
                                         <input type="text" name="email" class="form-control" placeholder="Email" value="" required="">
                                     </div>
