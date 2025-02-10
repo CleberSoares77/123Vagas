5a6a73d980e944075e21d16f9c726644238ea902
cleber <clebersoares749@gmail.com>
2025-02-10 15:12:58 -0300

    .

--- application/views/empresa/cadastro_empresa.php
+++ application/views/empresa/cadastro_empresa.php
@@ -61,51 +61,76 @@
 
     </style>
 
-    <div class="form-box">
-        <?php if (isset($empresa)) : ?>
-            <form method="post" action="<?= base_url('empresa/update_empresa/' . $empresa['id']); ?>">
-        <?php else : ?>
-            <form method="post" action="<?= base_url('empresa/cadastro_empresa'); ?>">
-        <?php endif; ?>
+<div class="form-box">
+  <?php if (isset($empresa)) : ?>
+    <form method="post" action="<?= base_url('empresa/update_empresa/' . $empresa['id']); ?>">
+  <?php else : ?>
+    <form method="post" action="<?= base_url('empresa/cadastro_empresa'); ?>">
+  <?php endif; ?>
 
-            <div class="mb-3 col-6">
-                <label for="nome" class="highlight-label">Nome:</label>
-                <input type="text" id="nome" name="nome" class="form-control" value="<?= isset($empresa) ? $empresa["nome"] : "" ?>">
-            </div>
-
-            <div class="mb-3 col-6">
-                <label for="cnpj" class="highlight-label">CNPJ:</label>
-                <input type="text" id="cnpj" name="cnpj" class="form-control" value="<?= isset($empresa) ? $empresa["cnpj"] : "" ?>">
-            </div>
-
-            <div class="mb-3 col-6">
-                <label for="contato" class="highlight-label">Contato:</label>
-                <input type="number" id="contato" name="contato" class="form-control" value="<?= isset($empresa) ? $empresa["contato"] : "" ?>">
-            </div>
-
-            <div class="mb-3 col-6">
-                <label for="email" class="highlight-label">Email:</label>
-                <input type="email" id="email" name="email" class="form-control" value="<?= isset($empresa) ? $empresa["email"] : "" ?>">
-            </div>
-
-            <div class="mb-3 col-6 position-relative">
-                <label for="senha" class="highlight-label">Senha:</label>
-                <div class="input-group">
-                    <input type="password" id="senha" name="senha" class="form-control" value="<?= isset($empresa) ? $empresa["senha"] : "" ?>">
-                    <div class="input-group-append">
-                        <span class="input-group-text">
-                            <i id="olho" class="fa fa-eye-slash eye-icon" onclick="togglePassword('senha')"></i>
-                        </span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="link">
-                <button type="submit" class="btn btn-primary mr-5">Salvar</button>
-                <a href="<?= base_url() ?>empresa" class="btn btn-danger btn-xs">Cancelar</a>
-            </div>
-        </form>
+    <!-- Campo Nome -->
+    <div class="mb-3 col-6">
+      <label for="nome" class="highlight-label">Nome:</label>
+      <input type="text" id="nome" name="nome" class="form-control"
+             value="<?= isset($empresa) ? $empresa["nome"] : "" ?>"
+             required
+             pattern="[A-Za-zÀ-ÿ\s]{2,}"
+             title="Digite um nome válido (apenas letras e espaços, no mínimo 2 caracteres).">
     </div>
+
+    <!-- Campo CNPJ -->
+    <div class="mb-3 col-6">
+      <label for="cnpj" class="highlight-label">CNPJ:</label>
+      <input type="text" id="cnpj" name="cnpj" class="form-control"
+             value="<?= isset($empresa) ? $empresa["cnpj"] : "" ?>"
+             required
+             pattern="\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}"
+             title="Digite um CNPJ válido no formato 00.000.000/0000-00.">
+    </div>
+
+    <!-- Campo Contato -->
+    <div class="mb-3 col-6">
+      <label for="contato" class="highlight-label">Contato:</label>
+      <input type="number" id="contato" name="contato" class="form-control"
+             value="<?= isset($empresa) ? $empresa["contato"] : "" ?>"
+             required
+             min="1000000000"
+             title="Digite um número de contato válido.">
+    </div>
+
+    <!-- Campo Email -->
+    <div class="mb-3 col-6">
+      <label for="email" class="highlight-label">Email:</label>
+      <input type="email" id="email" name="email" class="form-control"
+             value="<?= isset($empresa) ? $empresa["email"] : "" ?>"
+             required
+             title="Digite um email válido.">
+    </div>
+
+    <!-- Campo Senha -->
+    <div class="mb-3 col-6 position-relative">
+      <label for="senha" class="highlight-label">Senha:</label>
+      <div class="input-group">
+        <input type="password" id="senha" name="senha" class="form-control"
+               value="<?= isset($empresa) ? $empresa["senha"] : "" ?>"
+               required
+               minlength="6"
+               title="A senha deve ter no mínimo 6 caracteres.">
+        <div class="input-group-append">
+          <span class="input-group-text">
+            <i id="olho" class="fa fa-eye-slash eye-icon" onclick="togglePassword('senha')"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <div class="link">
+      <button type="submit" class="btn btn-primary mr-5">Salvar</button>
+      <a href="<?= base_url() ?>empresa" class="btn btn-danger btn-xs">Cancelar</a>
+    </div>
+  </form>
+</div>
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
